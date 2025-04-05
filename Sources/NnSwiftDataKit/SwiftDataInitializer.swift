5f91aba9ac790bdec4bc049f7f7bcaaf9e4de714
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-04-05 11:52:17 -0700

    fixed SwiftDataInitializer to work on scenes

---
+++ Sources/NnSwiftDataKit/SwiftDataInitializer.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftDataInitializer.swift
+//  NnSwiftDataKit
+//
+//  Created by Nikolai Nobadi on 4/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+extension Scene {
+    func initializeSwiftDataModelContainer(schema: Schema, migrationPlan: (any SchemaMigrationPlan.Type)? = nil, configuration: ModelConfiguration = .init(), printDatabasePath: Bool = false) -> some Scene {
+        do {
+            let container = try ModelContainer(for: schema, migrationPlan: migrationPlan, configurations: configuration)
+            
+            if printDatabasePath {
+                print(URL.applicationSupportDirectory.path(percentEncoded: false))
+            }
+            
+            return self.modelContainer(container)
+        } catch {
+            fatalError()
+        }
+    }
+}
